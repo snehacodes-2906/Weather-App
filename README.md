e7d041ee218432dd6c6eb5dc0a5934809c58b635
Sneha <malakarsneha36@gmail.com>
2026-07-16 22:55:17 +0530

    Initial Weather App

---
+++ README.md
@@ -0,0 +1,36 @@
+# Weather App
+
+## Description
+A simple weather application built using HTML, CSS, and JavaScript. It fetches real-time weather information using the OpenWeatherMap API.
+
+## Features
+- Search weather by city
+- Displays:
+  - Temperature
+  - Humidity
+  - Wind Speed
+  
+
+## Technologies Used
+- HTML5
+- CSS3
+- JavaScript
+- OpenWeatherMap API
+
+## How to Run
+1. Download the project.
+2. Open `script.js`.
+3. Replace `YOUR_API_KEY_HERE` with your OpenWeatherMap API key.
+4. Open `index.html` in your browser.
+
+## Project Structure
+
+Weather-App/
+├── index.html
+├── style.css
+├── script.js
+├── README.md
+└── images/
+
+## Author
+Sneha 
---
+++ script.js
@@ -0,0 +1,54 @@
+
+const apiKey="API_KEY";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox=document.querySelector(".search input");
+const searchBtn=document.querySelector(".search button");
+
+const weatherIcon=document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+	const response=await fetch(apiUrl+ city+ `&appid=${apiKey}`);
+	var data=await response.json();
+
+	if(data.cod == "404"){
+        alert("City not found");
+        return;
+    }
+
+	console.log(data);
+
+	document.querySelector(".city").innerHTML=data.name;
+	document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
+	document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+	document.querySelector(".wind").innerHTML=data.wind.speed+" km/h";
+
+
+	if(data.weather[0].main=="Clouds"){
+		weatherIcon.src= "images/clouds.png";
+	}
+	else if(data.weather[0].main=="Clear"){
+		weatherIcon.src= "images/sun_6974833.png";
+	}
+	else if(data.weather[0].main=="Rain"){
+		weatherIcon.src= "images/heavy-rain.png";
+	}
+	else if(data.weather[0].main=="Drizzle"){
+		weatherIcon.src= "images/icons8-drizzle-66.png";
+	}
+	else if(data.weather[0].main=="Mist"){
+		weatherIcon.src= "images/icons8-mist-24.png";
+	}
+	
+	
+   document.querySelector(".weather").style.display = "block";
+   console.log(data.weather[0].main);
+   console.log(data.weather[0].description);
+
+}
+
+searchBtn.addEventListener("click", ()=>{
+	checkWeather(searchBox.value);
+})
+
+checkWeather("London");
